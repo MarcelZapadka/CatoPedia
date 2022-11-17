a5433c30206218ba074699764bb8b005e99e063c
MarcelZapadka <zapadka.m@wp.pl>
2022-11-17 17:44:35 +0100

    CatoPedia

--- CatoPedia/src/app/Service/cat-info.service.ts
+++ CatoPedia/src/app/Service/cat-info.service.ts
@@ -5,7 +5,7 @@ export class CatInfoService {
 
     fetchCatsInfo() {
         return new Promise<void>((resolve, reject) => {
-            fetch('https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=1', {headers: {'x-api-key': 'live_QsczSa4NsqG8yZiUShaXZfYWfW4k6LZsN1MCEdxVSungCF1jmhR1WL37EgfcS6h1'} })
+            fetch('https://api.thecatapi.com/v1/images/search?limit=100&has_breeds=1', {headers: {'x-api-key': 'live_QsczSa4NsqG8yZiUShaXZfYWfW4k6LZsN1MCEdxVSungCF1jmhR1WL37EgfcS6h1'} })
             .then(response => response.json())
             .then(response => resolve(response))
         })
--- CatoPedia/src/app/main-page/main-page.component.ts
+++ CatoPedia/src/app/main-page/main-page.component.ts
@@ -9,9 +9,25 @@ import { CatInfoService } from '../Service/cat-info.service';
 
 export class MainPageComponent implements OnInit {
 
-  cats: any = [];
-  uniqeCats: any = [];
-
+  isCatStrangerFriendly: string | number = ''
+  catSocialNeeds: string | number = ''
+  catAdaptability: number | string = ''
+  catHealthIssues: string | number = ''
+  catWeightImperial: string = ''
+  catIntelligence: number | string = ''
+  isCatChildFriendly: string | number = ''
+  catAffectionLevel: string | number = '';
+  catOrigin: string = '';
+  catImageUrl: string = '';
+  allCats: any = [];
+  filteredCats: any = [];
+  catDescription: string = '';
+  catWeightMetric: string = '';
+  catName: string = '';
+  catTemperament: string = '';
+  catLifeSpan: string = '';
+  catWikiLink: string = '';
+  
   constructor(
     private catService: CatInfoService,
   ) { }
@@ -24,18 +40,87 @@ export class MainPageComponent implements OnInit {
 
   async getCatsInfo() {
     const catInfo = await this.catService.fetchCatsInfo();
-    this.cats = catInfo;
-    this.uniqeCats = [...this.cats.reduce((map: any, obj: any) => map.set(obj.breeds[0].name, obj), new Map()).values()];
-    console.log(this.uniqeCats);
+    this.allCats = catInfo;
+    this.filteredCats = [...this.allCats.reduce((map: any, obj: any) => map.set(obj.breeds[0].id, obj), new Map()).values()];
+    console.log(this.filteredCats);
   }
   
   displayCatInfo() {
-    for (let i = 0; i < 10; i++ ) {
+    for (let i = 0; i < 60; i++ ) {
       let catCardId = document.getElementById("cat-card-" + i);
-        catCardId?.addEventListener("click", () => {
-        console.log(this.uniqeCats[i].breeds[0].name)
+      let overlay = document.getElementById("overlay")!;
+      let catPopUp = document.getElementById("cat-popup");
+      catCardId?.addEventListener("click", () => {
+      this.catName = this.filteredCats[i].breeds[0].name;
+      this.catWeightMetric = this.filteredCats[i].breeds[0].weight.metric;
+      this.catDescription = this.filteredCats[i].breeds[0].description;
+      this.catTemperament = this.filteredCats[i].breeds[0].temperament;
+      this.catLifeSpan = this.filteredCats[i].breeds[0].life_span;
+      this.catImageUrl = this.filteredCats[i].url;
+      this.catOrigin = this.filteredCats[i].breeds[0].origin;
+      this.catIntelligence = this.filteredCats[i].breeds[0].intelligence;
+      this.isCatChildFriendly = this.filteredCats[i].breeds[0].child_friendly;
+      this.catAffectionLevel = this.filteredCats[i].breeds[0].affection_level;
+      this.catWeightImperial = this.filteredCats[i].breeds[0].weight.imperial;
+      this.catHealthIssues = this.filteredCats[i].breeds[0].health_issues;
+      this.catAdaptability = this.filteredCats[i].breeds[0].adaptability;
+      this.catSocialNeeds = this.filteredCats[i].breeds[0].social_needs;
+      this.isCatStrangerFriendly = this.filteredCats[i].breeds[0].stranger_friendly;
+      this.catWikiLink = this.filteredCats[i].breeds[0].wikipedia_url;
+      this.displayAdditionalCatInfo();
+      overlay.classList.add("active");
+      catPopUp?.classList.add("active");
+
       })
     }
+  }
+
+  displayAdditionalCatInfo() {
+    switch (
+      this.catIntelligence,
+      this.catAffectionLevel,
+      this.isCatChildFriendly,
+      this.catHealthIssues,
+      this.catAdaptability,
+      this.isCatStrangerFriendly,
+      this.catSocialNeeds) {
+      case 1:
+      case 2:
+        this.catIntelligence = "Low";
+        this.catAffectionLevel = "Low";
+        this.isCatChildFriendly = "Unfriendly";
+        this.catHealthIssues = "Very rarely";
+        this.catAdaptability = "Low";
+        this.isCatStrangerFriendly = "Distrustful"
+        this.catSocialNeeds = "Low"
+        break;
+      case 3:
+        this.catIntelligence = "Medium";
+        this.catAffectionLevel = "Medium";
+        this.isCatChildFriendly = "Neutral";
+        this.catHealthIssues = "Sometimes";
+        this.catAdaptability = "Medium";
+        this.isCatStrangerFriendly = "Neutral"
+        this.catSocialNeeds = "Medium"
+        break;
+      case 4:
+      case 5:
+        this.catIntelligence = "High";
+        this.catAffectionLevel = "High";
+        this.isCatChildFriendly = "Friendly";
+        this.catHealthIssues = "Very often";
+        this.catAdaptability = "High";
+        this.isCatStrangerFriendly = "Friendly"
+        this.catSocialNeeds = "High"
+        break;
+    }
+  }
+
+  hideCatInfo() {
+    let overlay = document.getElementById("overlay")!;
+    let catPopUp = document.getElementById("cat-popup");
+    overlay.classList.remove("active");
+    catPopUp?.classList.remove("active");
   }
 
   ngOnInit(): void {
