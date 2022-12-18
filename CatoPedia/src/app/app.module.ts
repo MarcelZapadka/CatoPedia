e3fe75c699cc8d8f08c5fa3712832095b89ddf78
MarcelZapadka <zapadka.m@wp.pl>
2022-12-18 14:40:21 +0100

    CatoPedia

--- CatoPedia/src/app/app.module.ts
+++ CatoPedia/src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainPageComponent } from './main-page/main-page.component';
-import {Ng2SearchPipeModule} from 'ng2-search-filter'
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainPageComponent,
+
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    Ng2SearchPipeModule,
     FormsModule,
   ],
   providers: [],
